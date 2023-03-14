fad5aef93c4b390bde9a53ac99f64d920ad2010f
zaiahkoh <zaiah0505@hotmail.com>
2023-03-14 16:27:24 -0700

    Added shared state.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use log;
 use pretty_env_logger;
-use std::error::Error;
+use std::{error::Error, sync::Mutex};
+use std::sync::Arc;
 use teloxide::{prelude::*, types::Me};
 
 #[tokio::main]
@@ -8,10 +9,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
     log::info!("Starting ");
 
+    let players = Arc::new(Mutex::new(Vec::<String>::new()));
+
     let bot = Bot::from_env();
     let handler = Update::filter_message().endpoint(msg_handler);
 
     Dispatcher::builder(bot, handler)
+        .dependencies(dptree::deps![players])
         .enable_ctrlc_handler()
         .build()
         .dispatch()
@@ -20,7 +24,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn msg_handler(bot: Bot, msg: Message, me: Me) -> Result<(), Box<dyn Error + Send + Sync>> {
-    bot.send_message(msg.chat.id, "Hello").await?;
+async fn msg_handler(
+    bot: Bot,
+    players: Arc<Mutex<Vec<String>>>,
+    msg: Message,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    players.lock().unwrap().push(String::from(msg.chat.username().unwrap()));
+    bot.send_message(msg.chat.id, format!("{:?}", players.lock().unwrap())).await?;
     Ok(())
 }
