579ed449b538018ecf0fc751f8641530f863ec97
zaiahkoh <zaiah0505@hotmail.com>
2023-03-14 16:00:12 -0700

    Basic replies.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
-use pretty_env_logger;
 use log;
+use pretty_env_logger;
+use std::error::Error;
+use teloxide::{prelude::*, types::Me};
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     pretty_env_logger::init();
     log::info!("Starting ");
+
+    let bot = Bot::from_env();
+    let handler = Update::filter_message().endpoint(msg_handler);
+
+    Dispatcher::builder(bot, handler)
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+
+    Ok(())
+}
+
+async fn msg_handler(bot: Bot, msg: Message, me: Me) -> Result<(), Box<dyn Error + Send + Sync>> {
+    bot.send_message(msg.chat.id, "Hello").await?;
+    Ok(())
 }
