463ed7079db8c1d45334ae6b9bf721327a00a7ff
zaiahkoh <zaiah0505@hotmail.com>
2023-03-16 15:13:23 -0700

    Squashed commit of the following:
    commit c137aced56ec6d6d9a3dd1c33f2321ffff638d9d
    Author: zaiahkoh <zaiah0505@hotmail.com>
    Date:   Thu Mar 16 15:11:33 2023 -0700
    
        Completed MVP
    
    commit cdb2a241f5cba656b867c6cf953441002cc416d4
    Author: zaiahkoh <zaiah0505@hotmail.com>
    Date:   Thu Mar 16 01:17:36 2023 -0700
    
        [WIP] adding commands.
    
    commit 80ce2e6b601d313d0e00f0d6b8db4a5d48f6c707
    Author: zaiahkoh <zaiah0505@hotmail.com>
    Date:   Thu Mar 16 00:21:57 2023 -0700
    
        [WIP] making MVP
    
    commit fad5aef93c4b390bde9a53ac99f64d920ad2010f
    Author: zaiahkoh <zaiah0505@hotmail.com>
    Date:   Tue Mar 14 16:27:24 2023 -0700
    
        Added shared state.
    
    commit 579ed449b538018ecf0fc751f8641530f863ec97
    Author: zaiahkoh <zaiah0505@hotmail.com>
    Date:   Tue Mar 14 16:00:12 2023 -0700
    
        Basic replies.
    
    commit 37dce06b2b711fa386a3c51625521dd09e19cd1e
    Author: zaiahkoh <zaiah0505@hotmail.com>
    Date:   Tue Mar 14 15:21:49 2023 -0700
    
        Add teloxide dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,104 @@
-fn main() {
-    println!("Hello, world!");
+use log;
+use pretty_env_logger;
+use std::sync::Arc;
+use std::{error::Error, sync::Mutex};
+use teloxide::{prelude::*, utils::command::BotCommands};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    pretty_env_logger::init();
+    log::info!("Starting That Mafia Game Bot");
+
+    let players = Arc::new(Mutex::new(Option::<Game>::None));
+
+    let handler = Update::filter_message().branch(
+        dptree::entry()
+            .filter_command::<StartCommand>()
+            .endpoint(start_command_handler),
+    );
+
+    let bot = Bot::from_env();
+    Dispatcher::builder(bot, handler)
+        .dependencies(dptree::deps![players])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+
+    Ok(())
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "lowercase", description = "Start commands")]
+enum StartCommand {
+    #[command(description = "shows this message.")]
+    Help,
+    #[command(description = "host a game")]
+    Host,
+    #[command(description = "join a game")]
+    Join { code: i32 },
+}
+
+async fn start_command_handler(
+    game: Arc<Mutex<Option<Game>>>,
+    bot: Bot,
+    msg: Message,
+    cmd: StartCommand,
+) -> Result<(), teloxide::RequestError> {
+    let text = match cmd {
+        StartCommand::Help => StartCommand::descriptions().to_string(),
+        StartCommand::Host => {
+            let mut g = game.lock().unwrap();
+            match g.as_ref() {
+                Some(g) => {
+                    format!("Existing game room: {}", g.code)
+                }
+                None => {
+                    let _ = g.insert(Game {
+                        host: msg.chat.id,
+                        players: vec![Player {
+                            id: msg.chat.id,
+                            username: String::from(msg.chat.username().unwrap()),
+                        }],
+                        code: 123,
+                    });
+                    format!("Creating new game: code={}", 123)
+                }
+            }
+        }
+        StartCommand::Join { code: c } => {
+            let mut g = game.lock().unwrap();
+            match g.as_mut() {
+                Some(Game {
+                    code: asdf,
+                    players: p,
+                    ..
+                }) if asdf.to_owned() == c => {
+                    p.push(Player {
+                        id: msg.chat.id,
+                        username: String::from(msg.chat.username().unwrap()),
+                    });
+                    format!("Joining game #{}", asdf)
+                }
+                _ => {
+                    format!("Cannot find game #{}", c)
+                }
+            }
+        }
+    };
+
+    bot.send_message(msg.chat.id, text).await?;
+
+    Ok(())
+}
+
+struct Player {
+    id: ChatId,
+    username: String,
+}
+
+struct Game {
+    host: ChatId,
+    players: Vec<Player>,
+    code: i32,
 }
