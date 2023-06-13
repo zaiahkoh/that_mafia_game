b0624b1ad3f6985df41db8b7569ad2bd31689393
zaiahkoh <zaiah0505@hotmail.com>
2023-06-13 22:18:53 +0800

    yeboiii

--- src/lib.rs
+++ src/lib.rs
@@ -30,13 +30,11 @@ pub async fn start_mafia_bot() -> Result<(), Box<dyn Error>> {
         lobby_manager: LocalLobbyManager::new(),
     }));
 
-    let handler = Update::filter_message()
-        .branch(
-            dptree::entry()
-                .filter_command::<MainMenuCommand>()
-                .endpoint(main_menu_handler),
-        )
-        .branch(get_lobby_handler());
+    let handler = Update::filter_message().branch(get_lobby_handler()).branch(
+        dptree::entry()
+            .filter_command::<MainMenuCommand>()
+            .endpoint(main_menu_handler),
+    );
 
     let bot = Bot::from_env();
     Dispatcher::builder(bot, handler)
@@ -65,13 +63,11 @@ fn get_lobby_handler() -> Handler<
     teloxide::dispatching::DpHandlerDescription,
 > {
     dptree::filter(lobby_check)
+        .filter_command::<LobbyCommand>()
         .endpoint(lobby_handler)
 }
 
-fn lobby_check(
-    msg: Message,
-    bot_state: AsyncBotState,
-) -> bool {
+fn lobby_check(msg: Message, bot_state: AsyncBotState) -> bool {
     let mut state_lock = bot_state.lock().unwrap();
     if let Some(_) = state_lock.lobby_manager.get_chats_lobby(msg.chat.id) {
         return true;
@@ -84,8 +80,21 @@ async fn lobby_handler(
     bot_state: AsyncBotState,
     bot: Bot,
     msg: Message,
-    cmd: MainMenuCommand,
+    cmd: LobbyCommand,
 ) -> Result<(), teloxide::RequestError> {
+    let text = match cmd {
+        LobbyCommand::Help => LobbyCommand::descriptions().to_string(),
+        LobbyCommand::Quit => {
+            // let mut state_lock = bot_state.lock().unwrap();
+            // let mut lobby_manager = state_lock.lobby_manager;
+            // let lobby = lobby_manager.get_chats_lobby(msg.chat.id).unwrap();
+            // lobby_manager.quit_lobby(msg.chat.id);
+            format!("Quited lobby: {}", 123)
+        },        
+    };
+
+    bot.send_message(msg.chat.id, text).await?;
+
     Ok(())
 }
 
