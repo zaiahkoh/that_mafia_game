220270a44a2d53ccd1a24adf98cbb590c210f43b
zaiahkoh <zaiah@DESKTOP-1I4OF7F>
2023-07-14 17:16:06 +0800

    Added dotenv for retrieving TELOXIDE_TOKEN

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use dotenv::dotenv;
 use std::error::Error;
 use that_mafia_game::start_mafia_bot;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok();
     start_mafia_bot().await
 }
