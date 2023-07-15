ec54dc7fed8ccdd4928b9cf8f1926022ca67e77b
zaiahkoh <zaiah0505@hotmail.com>
2023-07-15 09:57:54 +0800

    [WIP] creating game_interface

--- src/game_interface.rs
+++ src/game_interface.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 
-use teloxide::types::ChatId;
+use teloxide::types::{ChatId, MessageId};
 
-use crate::lobby_manager::Lobby;
+use crate::{
+    game::game_phase::{Action, GamePhase},
+    lobby_manager::Lobby,
+};
 
 pub struct Player {
     chat_id: ChatId,
@@ -12,9 +15,36 @@ pub struct Player {
 pub trait Game {
     fn from_lobby(lobby: &Lobby) -> Self;
 
-    fn get_night_targets(&self) -> HashMap<ChatId, Vec<&Player>>;
+    fn get_phase(&self) -> GamePhase;
 
-    fn add_action(&mut self, action: Action) -> Result<(), &'static str>;
+    /// Attempts to end the phase. Returns Some(GamePhase) if the phase ended
+    fn end_phase(&mut self) -> Option<GamePhase>;
 
-    fn end_phase(&mut self);
+    /// Returns the most recent transition message
+    fn get_transition_message(&self) -> String;
+
+    /// Panics if the game is not in GamePhase::Night or if the action is illegal
+    fn add_night_action(&mut self, action: Action) -> Result<(), &'static str>;
+
+    /// Panics if the game is not in GamePhase::Voting
+    fn get_vote_options(&self) -> dyn Iterator<Item = Player>;
+
+    /// Panics if the game is not in GamePhase::Voting
+    fn get_voters(&self) -> dyn Iterator<Item = Player>;
+
+    /// Panics if the game is not in GamePhase::Voting. The poll_msg_ids HashMap should map the
+    /// voter's chat id to the message id of the poll.
+    fn add_vote_msg_ids(&mut self, poll_msg_ids: HashMap<ChatId, MessageId>);
+
+    /// Panics if the game is not in GamePhase::Voting. The chosen vector should contain the index of the options
+    /// as they appear in `get_vote_options`.
+    fn add_votes(&mut self, voter_id: ChatId, chosen: Vec<i32>);
+
+    /// Panics if the game is not in GamePhase::Trial
+    fn get_verdict_options(&self) -> dyn Iterator<Item = String>;
+
+    /// Panics if the game is not in GamePhase::Trial
+    fn get_jury(&self) -> dyn Iterator<Item = Player>;
+
+    
 }
